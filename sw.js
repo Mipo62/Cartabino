480e03c0ccba0c49e20cff0159d56fc7058ff030
MiP0 <128356837+Mipo62@users.noreply.github.com>
2026-01-11 21:37:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cartabino-v1';
+const CACHE_NAME = 'cartabino-v2';
 const ASSETS = [
   'index.html',
   'poster.png',
@@ -6,16 +6,14 @@ const ASSETS = [
   'manifest.json'
 ];
 
-// Installation du Service Worker et mise en cache des fichiers
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Récupération des fichiers depuis le cache si on est hors-ligne
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
